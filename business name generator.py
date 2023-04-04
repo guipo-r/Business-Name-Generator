0537cfc4bfd642cf1dc8668856bc4878763e1953
guipo <117786253+guipo-r@users.noreply.github.com>
2023-04-04 12:52:37 -0300

    Adding python script
    Hey there! Are you starting a brand new business but you ran of ideas for what its name should be? Suffer no more! Here's the solution for you! Just follow the instructions and a brand new name will be suggested!

---
+++ business name generator.py
@@ -0,0 +1,18 @@
+#1. Create a greeting for your program.
+print('Hey there! Welcome!')
+#2. Ask the user for the city that they grew up in.
+
+city = input('In what city did you grow up?\n')
+
+#3. Ask the user for the name of a pet.
+
+pet = input('What is or was the name of your pet?\n')
+
+#4. Combine the name of their city and pet and show them their band name.
+
+print('You might want to name your band as:\n'
+     + city + ' ' + pet)
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
